1060927a910f70a8ebd3051cf59b960c682473c5
Julian <jjvll90@gmail.com>
2024-05-16 20:35:12 -0500

    server.js and DB

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/records.js";
+
+// Path: server/routes/records.js
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+//start the Express server
+app.listen(PORT, () => {
+    console.log(`server started at http://localhost:${PORT}`);
+});
